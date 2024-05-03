2972f320ec491a3c4d00abd75e5093bfd98d44d2
YehorYehorychev <egor57899@gmail.com>
2024-05-03 16:46:14 -0700

    Guest Checkout Using Direct Bank Transfer test added (First iteration)

--- src/test/java/org/selenium/myFirstTestCase.java
+++ src/test/java/org/selenium/myFirstTestCase.java
@@ -1,12 +1,29 @@
 package org.selenium;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class myFirstTestCase {
     @Test
-    public void dummyTest() {
+    public void guestCheckoutUsingDirectBankTransfer() {
+        WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
+        driver.get("https://askomdch.com");
+        driver.manage().window().maximize();
+        driver.findElement(By.cssSelector("#menu-item-1227 > a")).click();
+        driver.findElement(By.id("woocommerce-product-search-field-0")).sendKeys("Blue");
+        driver.findElement(By.cssSelector("button[value='Search']")).click();
+        Assert.assertEquals(driver.findElement(By.cssSelector(".woocommerce-products-header__title.page-title")).getText(), "Search results: “Blue”");
+        driver.findElement(By.cssSelector("a[aria-label='Add “Blue Shoes” to your cart']")).click();
+        driver.findElement(By.cssSelector("a[title='View cart']")).click();
+        Assert.assertEquals(driver.findElement(By.cssSelector("td[class='product-name'] a")).getText(), "Blue Shoes");
+        driver.findElement(By.cssSelector(".checkout-button")).click();
+
+
+        //        driver.quit();
     }
 }
