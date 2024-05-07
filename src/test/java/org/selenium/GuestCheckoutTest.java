3fbb58a399c5aa249c973b4e8138023bef752868
YehorYehorychev <egor57899@gmail.com>
2024-05-06 18:12:27 -0700

    POM implementation (First iteration)

--- src/test/java/org/selenium/GuestCheckoutTest.java
+++ src/test/java/org/selenium/GuestCheckoutTest.java
@@ -5,28 +5,13 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.*;
 import org.openqa.selenium.support.ui.*;
 
+import org.selenium.pom.BaseTest;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
 import java.time.Duration;
 
-public class GuestCheckoutTest {
-
-    private WebDriver driver;
-    private WebDriverWait wait;
-
-    @BeforeClass
-    public void setUpClass() {
-        WebDriverManager.chromedriver().setup();
-    }
-
-    @BeforeMethod
-    public void setUp() {
-        driver = new ChromeDriver();
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        driver.manage().window().maximize();
-        driver.get("https://askomdch.com");
-    }
+public class GuestCheckoutTest  extends BaseTest {
 
     @Test
     public void guestCheckoutUsingDirectBankTransfer() {
@@ -135,15 +120,5 @@ public class GuestCheckoutTest {
     private void verifyOrderConfirmation() {
         WebElement wooNotice = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".woocommerce-notice")));
         Assert.assertEquals(wooNotice.getText(), "Thank you. Your order has been received.");
-    }
-
-    @AfterMethod
-    public void tearDown() {
-        driver.quit();
-    }
-
-    @AfterClass
-    public void tearDownClass() {
-        WebDriverManager.chromedriver().clearDriverCache();
     }
 }
---
+++ src/test/java/org/selenium/pom/BaseTest.java
@@ -0,0 +1,32 @@
+package org.selenium.pom;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.selenium.factory.DriverManager;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
+
+public class BaseTest {
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+
+    @BeforeMethod
+    public void startDriver() {
+        driver = new DriverManager().intializeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    @AfterMethod
+    public void quitDriver() {
+        driver.quit();
+    }
+
+    @AfterClass
+    public void tearDownClass() {
+        WebDriverManager.chromedriver().clearDriverCache();
+    }
+}
