c4be2cb2cf711206d2bce925a4e851435347971e
YehorYehorychev <egor57899@gmail.com>
2024-05-08 19:03:04 -0700

    loginToExistingAccountAndCheckoutUsingDirectBankTransfer() has been changed and refactored

--- src/test/java/org/selenium/GuestCheckoutTest.java
+++ src/test/java/org/selenium/GuestCheckoutTest.java
@@ -24,20 +24,47 @@ public class GuestCheckoutTest extends BaseTest {
         Assert.assertEquals(cartPage.getProductName(), "Blue Shoes");
 
         CheckoutPage checkoutPage = cartPage.checkout();
-        checkoutPage
-                .enterFirstName("Demo")
-                .enterLastName("QA")
-                .enterAddressLineOne("San Francisco")
-                .enterCity("San Francisco")
-                .enterPostCode("94040")
-                .enterEmail("yehor@test.com")
-                .placeOrder();
+        checkoutPage.
+                enterFirstName("Demo").
+                enterLastName("QA").
+                enterAddressLineOne("San Francisco").
+                enterCity("San Francisco").
+                enterPostCode("94040").
+                enterEmail("yehor@test.com").
+                placeOrder();
+
         Thread.sleep(1000);
         Assert.assertEquals(checkoutPage.getNotice(), "Thank you. Your order has been received.");
     }
 
     @Test
-    public void loginToExistingAccountAndCheckoutUsingDirectBankTransfer() {
+    public void loginToExistingAccountAndCheckoutUsingDirectBankTransfer() throws InterruptedException {
+        HomePage homePage = new HomePage(driver);
+        StorePage storePage = homePage.navigateToStoreUsingMenu();
+        storePage.search("Blue");
+        Assert.assertEquals(storePage.getTitle(), "Search results: “Blue”");
+        Thread.sleep(1000);
 
+        storePage.clickAddToCartButton("Blue Shoes");
+        Thread.sleep(2000);
+        CartPage cartPage = storePage.clickViewCart();
+        Assert.assertEquals(cartPage.getProductName(), "Blue Shoes");
+
+        CheckoutPage checkoutPage = cartPage.checkout();
+        checkoutPage.clickHereToLoginLink();
+        Thread.sleep(1000);
+
+        checkoutPage.
+                login("yehor12a", "DemoPassword").
+                enterFirstName("Demo").
+                enterLastName("QA").
+                enterAddressLineOne("San Francisco").
+                enterCity("San Francisco").
+                enterPostCode("94040").
+                enterEmail("yehor12a@gmail.com").
+                placeOrder();
+
+        Thread.sleep(1000);
+        Assert.assertEquals(checkoutPage.getNotice(), "Thank you. Your order has been received.");
     }
 }
--- src/test/java/org/selenium/pom/pages/CheckoutPage.java
+++ src/test/java/org/selenium/pom/pages/CheckoutPage.java
@@ -15,36 +15,47 @@ public class CheckoutPage extends BasePage {
     private final By placeOrderButton = By.cssSelector("#place_order");
     private final By successNotice = By.cssSelector(".woocommerce-notice");
 
+    private final By clickHereToLoginLink = By.cssSelector(".showlogin");
+    private final By usernameFiled = By.cssSelector("#username");
+    private final By passwordFiled = By.cssSelector("#password");
+    private final By loginButton = By.cssSelector("button[value='Login']");
+
     public CheckoutPage(WebDriver driver) {
         super(driver);
     }
 
     public CheckoutPage enterFirstName(String firstName) {
+        driver.findElement(firstNameField).clear();
         driver.findElement(firstNameField).sendKeys(firstName);
         return this;
     }
 
     public CheckoutPage enterLastName(String lastName) {
+        driver.findElement(lastNameField).clear();
         driver.findElement(lastNameField).sendKeys(lastName);
         return this;
     }
 
     public CheckoutPage enterAddressLineOne(String addressLineOne) {
+        driver.findElement(addressLineOneField).clear();
         driver.findElement(addressLineOneField).sendKeys(addressLineOne);
         return this;
     }
 
     public CheckoutPage enterCity(String city) {
+        driver.findElement(billingCityField).clear();
         driver.findElement(billingCityField).sendKeys(city);
         return this;
     }
 
     public CheckoutPage enterPostCode(String postCode) {
+        driver.findElement(billingPostCodeField).clear();
         driver.findElement(billingPostCodeField).sendKeys(postCode);
         return this;
     }
 
     public CheckoutPage enterEmail(String email) {
+        driver.findElement(billingEmailField).clear();
         driver.findElement(billingEmailField).sendKeys(email);
         return this;
     }
@@ -57,5 +68,33 @@ public class CheckoutPage extends BasePage {
 
     public String getNotice() {
         return driver.findElement(successNotice).getText();
+    }
+
+    public CheckoutPage clickHereToLoginLink() {
+        driver.findElement(clickHereToLoginLink).click();
+        return this;
+    }
+
+    public CheckoutPage enterUserName(String username) {
+        driver.findElement(usernameFiled).clear();
+        driver.findElement(usernameFiled).sendKeys(username);
+        return this;
+    }
+
+    public CheckoutPage enterPassword(String password) {
+        driver.findElement(passwordFiled).clear();
+        driver.findElement(passwordFiled).sendKeys(password);
+        return this;
+    }
+
+    public CheckoutPage clickLoginButton() {
+        driver.findElement(loginButton).click();
+        return this;
+    }
+
+    public CheckoutPage login(String username, String password) {
+        return enterUserName(username).
+                enterPassword(password).
+                clickLoginButton();
     }
 }
