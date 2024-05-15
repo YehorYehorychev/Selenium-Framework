1268a84d44baf7309bb54bd437af531ce626dd4e
YehorYehorychev <egor57899@gmail.com>
2024-05-14 19:39:00 -0700

    Driver manager class has been refactored

--- src/test/java/org/selenium/pom/factory/DriverManager.java
+++ src/test/java/org/selenium/pom/factory/DriverManager.java
@@ -1,5 +1,6 @@
 package org.selenium.pom.factory;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import java.time.Duration;
@@ -7,6 +8,7 @@ import java.time.Duration;
 public class DriverManager {
 
     public WebDriver initializeDriver() {
+        WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
      //   driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
