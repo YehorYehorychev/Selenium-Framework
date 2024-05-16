6bfeb2a4824fb71d7ba4aaacc484d8183af66c0d
YehorYehorychev <egor57899@gmail.com>
2024-05-16 16:51:59 -0700

    Browser Switcher added to the Drivermanager class

--- src/test/java/org/selenium/pom/factory/DriverManager.java
+++ src/test/java/org/selenium/pom/factory/DriverManager.java
@@ -5,14 +5,23 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-import java.time.Duration;
 
 public class DriverManager {
 
     public WebDriver initializeDriver() {
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        // FireFox driver setup -> WebDriverManager.firefoxdriver().setup(); WebDriver driver = new FirefoxDriver();
+        WebDriver driver;
+        String browser = System.getProperty("browser");
+        switch (browser) {
+            case "Chrome" -> {
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+            }
+            case "Firefox" -> {
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+            }
+            default -> throw new IllegalStateException("Invalid browser name: " + browser);
+        }
         driver.manage().window().maximize();
         return driver;
     }
