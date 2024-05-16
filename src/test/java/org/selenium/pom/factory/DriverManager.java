1bfe43fe3d6c3616613c3458e4447fbc4db78c80
YehorYehorychev <egor57899@gmail.com>
2024-05-16 16:30:11 -0700

    Firefox driver setup has been added

--- src/test/java/org/selenium/pom/factory/DriverManager.java
+++ src/test/java/org/selenium/pom/factory/DriverManager.java
@@ -3,6 +3,8 @@ package org.selenium.pom.factory;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 import java.time.Duration;
 
 public class DriverManager {
@@ -10,8 +12,8 @@ public class DriverManager {
     public WebDriver initializeDriver() {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
+        // FireFox driver setup -> WebDriverManager.firefoxdriver().setup(); WebDriver driver = new FirefoxDriver();
         driver.manage().window().maximize();
-     //   driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         return driver;
     }
 }
