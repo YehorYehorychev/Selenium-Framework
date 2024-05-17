c312ce832536ad9ac22faf58c17cd641d331a0b3
YehorYehorychev <egor57899@gmail.com>
2024-05-16 17:47:04 -0700

    DriverManager class drivers instruction added

--- src/test/java/org/selenium/pom/factory/DriverManager.java
+++ src/test/java/org/selenium/pom/factory/DriverManager.java
@@ -11,7 +11,13 @@ import static org.selenium.pom.constants.DriverType.*;
 
 
 public class DriverManager {
-    // To run your tests in specific browser run -> mvn clean test -Dbrowser=CHROME
+    /*
+        To run your tests in specific browser run -> mvn clean test -Dbrowser=CHROME
+        ---------------------------------------------------------------------------------------
+        If you prefer to run your tests directly from the IDE instead of using "mvn clean test",
+        you need to set the system property as follows ->
+        For example: String browser = System.getProperty("browser", "CHROME");
+    */
     public WebDriver initializeDriver() {
         WebDriver driver;
         String browser = System.getProperty("browser");
