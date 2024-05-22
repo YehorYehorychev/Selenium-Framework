7bd46e7560c3c563d48d7a7ff2d8ed04b36583d6
YehorYehorychev <egor57899@gmail.com>
2024-05-21 18:53:23 -0700

    ConfigLoader() class created

--- src/test/java/org/selenium/pom/utils/JacksonUtils.java
+++ src/test/java/org/selenium/pom/utils/JacksonUtils.java
@@ -1,9 +1,6 @@
 package org.selenium.pom.utils;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.selenium.pom.objects.BillingAddress;
-
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -12,6 +9,7 @@ public class JacksonUtils {
     public static <T> T deserializeJson(String fileName, Class<T> T) throws IOException {
         InputStream is = JacksonUtils.class.getClassLoader().getResourceAsStream(fileName);
         ObjectMapper objectMapper = new ObjectMapper();
+        ConfigLoader configLoader = new ConfigLoader();
         return objectMapper.readValue(is, T);
     }
 }
