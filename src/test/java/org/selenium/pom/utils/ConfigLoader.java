6e549a102d1a7adf4df73b0687ef44d44354aeb4
YehorYehorychev <egor57899@gmail.com>
2024-05-22 17:17:21 -0700

    Config properties files have been refactored.

--- src/test/java/org/selenium/pom/utils/ConfigLoader.java
+++ src/test/java/org/selenium/pom/utils/ConfigLoader.java
@@ -7,7 +7,7 @@ public class ConfigLoader {
     private static ConfigLoader configLoader;
 
     private ConfigLoader() {
-        properties = PropertyUtils.propertyLoader("src/test/resources/config.properties");
+        properties = PropertyUtils.propertyLoader("src/test/resources/stg_config.properties");
     }
 
     public static ConfigLoader getInstance() {
@@ -20,17 +20,17 @@ public class ConfigLoader {
     public String getBaseUrl() {
         String prop = properties.getProperty("baseUrl");
         if (prop != null) return prop;
-        else throw new RuntimeException("Property baseUrl is not specified in the config.properties file");
+        else throw new RuntimeException("Property baseUrl is not specified in the stg_config.properties file");
     }
 /*    public String getLogin() {
         String prop = properties.getProperty("login");
         if (prop != null) return prop;
-        else throw new RuntimeException("Property login is not specified in the config.properties file");
+        else throw new RuntimeException("Property login is not specified in the stg_config.properties file");
     }
 
     public String getPassword() {
         String prop = properties.getProperty("password");
         if (prop != null) return prop;
-        else throw new RuntimeException("Property password is not specified in the config.properties file");
+        else throw new RuntimeException("Property password is not specified in the stg_config.properties file");
     }*/
 }
