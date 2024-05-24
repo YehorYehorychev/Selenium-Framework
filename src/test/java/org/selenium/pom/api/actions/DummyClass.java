27637dbb90010db9837d436ead07564dcefca163
YehorYehorychev <egor57899@gmail.com>
2024-05-23 17:39:43 -0700

    SignUpApi test and DummyClass has been refactored

--- src/test/java/org/selenium/pom/api/actions/DummyClass.java
+++ src/test/java/org/selenium/pom/api/actions/DummyClass.java
@@ -3,6 +3,6 @@ package org.selenium.pom.api.actions;
 public class DummyClass {
 
     public static void main(String[] args) {
-        new SignUpApi().getAccount();
+        System.out.println(new SignUpApi().fetchRegisterNonceValue());
     }
 }
