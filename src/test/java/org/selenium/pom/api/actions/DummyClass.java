7e39df7c5c5f570bf1f5e6abe4fc7fb29fc25c7e
YehorYehorychev <egor57899@gmail.com>
2024-05-24 18:16:20 -0700

    User registration and addToCart merged to the one e2e test in Dummy class

--- src/test/java/org/selenium/pom/api/actions/DummyClass.java
+++ src/test/java/org/selenium/pom/api/actions/DummyClass.java
@@ -7,16 +7,15 @@ import org.selenium.pom.utils.FakerUtils;
 public class DummyClass {
 
     public static void main(String[] args) {
-/*        String login = "demoqa" + new FakerUtils().generateRandomNumber();
+        String login = "demoqa" + new FakerUtils().generateRandomNumber();
         UserData userData = new UserData().
                 setLogin(login).
                 setPassword("demopwd").
                 setEmail(login + "@gmail.com");
         SignUpApi signUpApi = new SignUpApi();
-        System.out.println(signUpApi.register(userData));
-        System.out.println(signUpApi.getCookies());*/
-
-        CartApi cartApi = new CartApi();
+        signUpApi.register(userData);
+        System.out.println("REGISTER COOKIES: " + signUpApi.getCookies());
+        CartApi cartApi = new CartApi(signUpApi.getCookies());;;
         cartApi.addToCart(1215, 1);
         System.out.println(cartApi.getCookies());
     }
