d7bba4a18a8798aa6bdf22dcf2d4b443bffdaa70
YehorYehorychev <egor57899@gmail.com>
2024-05-24 18:35:36 -0700

    First iteration of loginDuringCheckout() has been created in LoginTest class

---
+++ src/test/java/org/selenium/pom/tests/LoginTest.java
@@ -0,0 +1,28 @@
+package org.selenium.pom.tests;
+
+import org.selenium.pom.api.actions.CartApi;
+import org.selenium.pom.api.actions.SignUpApi;
+import org.selenium.pom.objects.Product;
+import org.selenium.pom.objects.UserData;
+import org.selenium.pom.utils.FakerUtils;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class LoginTest {
+
+    @Test
+    public void loginDuringCheckout() throws IOException {
+        String username = "demoqa" + new FakerUtils().generateRandomNumber();
+        UserData userData = new UserData().
+                setLogin(username).
+                setPassword("demopwd").
+                setEmail(username + "@gmail.com");
+
+        SignUpApi signUpApi = new SignUpApi();
+        signUpApi.register(userData);
+        CartApi cartApi = new CartApi();
+        Product product = new Product(1215);
+        cartApi.addToCart(product.getId(), 1);
+    }
+}
