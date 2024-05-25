651174196ccf28e2ddfa031573b47447272cda16
YehorYehorychev <egor57899@gmail.com>
2024-05-24 17:16:54 -0700

    CartApi class created

--- src/test/java/org/selenium/pom/api/actions/CartApi.java
+++ src/test/java/org/selenium/pom/api/actions/CartApi.java
@@ -4,55 +4,15 @@ import io.restassured.http.Cookies;
 import io.restassured.http.Header;
 import io.restassured.http.Headers;
 import io.restassured.response.Response;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.selenium.pom.objects.UserData;
 import org.selenium.pom.utils.ConfigLoader;
 
 import java.util.HashMap;
 
 import static io.restassured.RestAssured.given;
 
-public class SignUpApi {
-    private Cookies cookies;
+public class CartApi {
 
-    public Cookies getCookies() {
-        return cookies;
-    }
-
-    private String fetchRegisterNonceValueUsingGroovy() {
-        Response response = getAccount();
-        return response.htmlPath().getString("**.findAll { it.@name == 'woocommerce-register-nonce' }.@value");
-    }
-
-    private String fetchRegisterNonceValueUsingJsoup() {
-        Response response = getAccount();
-        Document document = Jsoup.parse(response.body().prettyPrint());
-        Element element = document.selectFirst("#woocommerce-register-nonce");
-        assert element != null;
-        return element.attr("value");
-    }
-
-    private Response getAccount() {
-        Response response =
-                given()
-                        .baseUri(ConfigLoader.getInstance().getBaseUrl())
-                        .log().all()
-                        .when()
-                        .get("/account")
-                        .then()
-                        .log().all()
-                        .extract()
-                        .response();
-        if (response.getStatusCode() != 200) {
-            throw new RuntimeException("Failed to fetch the account " + response.getStatusCode());
-        }
-        return response;
-    }
-
-    public Response register(UserData userData) {
-        Cookies cookies = new Cookies();
+    public Response addToCart() {
         Header header = new Header("content-type", "application/x-www-form-urlencoded");
         Headers headers = new Headers(header);
         HashMap<String, String> formParams = new HashMap<>();
--- src/test/java/org/selenium/pom/api/actions/DummyClass.java
+++ src/test/java/org/selenium/pom/api/actions/DummyClass.java
@@ -8,10 +8,10 @@ public class DummyClass {
 
     public static void main(String[] args) {
         String login = "demoqa" + new FakerUtils().generateRandomNumber();
-        UserData userData = new UserData()
-                .setLogin(login)
-                .setPassword("demopwd")
-                .setEmail(login + "@gmail.com");
+        UserData userData = new UserData().
+                setLogin(login).
+                setPassword("demopwd").
+                setEmail(login + "@gmail.com");
         SignUpApi signUpApi = new SignUpApi();
         System.out.println(signUpApi.register(userData));
         System.out.println(signUpApi.getCookies());
