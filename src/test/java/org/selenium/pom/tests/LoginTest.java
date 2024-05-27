f2e59adc6df55d422764cd789b625b86d272996b
YehorYehorychev <egor57899@gmail.com>
2024-05-27 08:53:21 -0700

    LoginTest class first iteration of login in account via API requrst implemented

--- src/test/java/org/selenium/pom/tests/LoginTest.java
+++ src/test/java/org/selenium/pom/tests/LoginTest.java
@@ -5,6 +5,7 @@ import org.selenium.pom.api.actions.SignUpApi;
 import org.selenium.pom.base.BaseTest;
 import org.selenium.pom.objects.Product;
 import org.selenium.pom.objects.UserData;
+import org.selenium.pom.pages.CheckoutPage;
 import org.selenium.pom.utils.FakerUtils;
 import org.testng.annotations.Test;
 
@@ -15,10 +16,7 @@ public class LoginTest extends BaseTest {
     @Test
     public void loginDuringCheckout() throws IOException {
         String username = "demoqa" + new FakerUtils().generateRandomNumber();
-        UserData userData = new UserData().
-                setLogin(username).
-                setPassword("demopwd").
-                setEmail(username + "@gmail.com");
+        UserData userData = new UserData().setLogin(username).setPassword("demopwd").setEmail(username + "@gmail.com");
 
         SignUpApi signUpApi = new SignUpApi();
         signUpApi.register(userData);
@@ -26,5 +24,10 @@ public class LoginTest extends BaseTest {
         Product product = new Product(1215);
         cartApi.addToCart(product.getId(), 1);
         injectCookiesToBrowser(cartApi.getCookies());
+
+        CheckoutPage checkoutPage = new CheckoutPage(getDriver()).
+                load().
+                clickHereToLoginLink().
+                setUserCredentials(userData);
     }
 }
